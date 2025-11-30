493670f2ea7c5363faaa6b845dcbc90131573f51
Abimael Garcia <abimael1992g@gmail.com>
2025-11-29 21:07:06 -0600

    refactor update vendor analytics view for cleaner presentation

--- app/vendor/analytics/page.tsx
+++ app/vendor/analytics/page.tsx
@@ -1,11 +1,11 @@
-// app/vendor/analytics/page.tsx
-import { VendorAnalytics } from "@/components/vendor-dashboard/analytics-dashboard"
-import { VendorLayout } from "@/components/layout/vendor-layout"
+// // app/vendor/analytics/page.tsx
+// import { VendorAnalytics } from "@/components/vendor-dashboard/analytics-dashboard"
+// import { VendorLayout } from "@/components/layout/vendor-layout"
 
-export default function VendorAnalyticsPage() {
-    return (
-        <VendorLayout>
-            <VendorAnalytics />
-        </VendorLayout>
-    )
-}
+// export default function VendorAnalyticsPage() {
+//     return (
+//         <VendorLayout>
+//             <VendorAnalytics />
+//         </VendorLayout>
+//     )
+// }
