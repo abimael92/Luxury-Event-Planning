9f8035f4773e311ae2db98169b68e02f538fe247
Abimael Garcia <abimael1992g@gmail.com>
2025-12-12 00:33:07 -0600

    refactor unify UI component behavior, styling, and patterns

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { CheckIcon } from 'lucide-react'
 
-import { cn } from '@/shared/lib/utils' // src/shared/lib/utils.ts
+import { cn } from '../../app/shared/lib/utils' // src/shared/lib/utils.ts
 
 function Checkbox({
   className,
